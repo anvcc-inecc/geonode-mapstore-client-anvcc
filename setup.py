4825b3170e9ee2b1915e6266423d07e5fda0611d
Niji <mathereall@gmail.com>
2023-12-29 22:34:45 -0600

    correcciones para compatibilidad anvcc

--- setup.py
+++ setup.py
@@ -13,14 +13,37 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
+URL = "https://github.com/anvcc-inecc/geonode-mapstore-client-anvcc"
+DESCRIPTION = "Use GeoNode client in your django projects"
 
 def get_version():
-    version = open('VERSION','r').read()
-    version = version.replace('\n','')
+    version = open('VERSION', 'r').read()
+    version = version.replace('\n', '')
     return version
 
+
+
+
+
+
 setup(
+    name="django-geonode-mapstore-client",
     version=get_version(),
+    url=URL,
+    description=DESCRIPTION,
     long_description=open(os.path.join(here, 'README.md')).readline(),
     long_description_content_type='text/markdown',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP",
+        "Programming Language :: Python :: 3.10",
+    ],
+    install_requires=open(os.path.join(here, 'install_requires.txt')).readlines(),
 )
+
+VERSION = get_version()
